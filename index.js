44560873c820790f1993944b37e3b2f4015dad8b
UKReactDev <uk.umut873@gmail.com>
2022-11-13 15:31:18 +0300

    Odev 13
    Odev 13

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 let saveEl = document.getElementById("save-el")
 let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+
 if( gecmisBilgi !== null) {
     saveEl.textContent = localStorage.getItem("gecmisBilgi")
 } else {
     saveEl.textContent = ""
 }
 
-
+let total = document.querySelector(".total-number")
 let countEl = document.getElementById("count-el")
 let count = 0
+let result = 0;
 
 function increment() {
     count += 1
     countEl.textContent = count
+
 }
 
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
-    localStorage.setItem( "gecmisBilgi", saveEl.textContent )
-    countEl.textContent = 0
-    count = 0
-}
+    result += count
+    total.textContent = result
 
+    localStorage.setItem( "gecmisBilgi", saveEl.textContent )
+
+     countEl.textContent = 0
+     count = 0
+ 
+}
